187911fd3bbcf2f0ff0c27eb70daf13590df8732
cynddl <luc@rocher.lc>
2017-05-07 16:59:27 +0200

    Cleanup js code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,16 @@
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY3luZGRsIiwiYSI6ImNqMmQ0eWh1aTAwMTcyeG11b3lndWVkc3YifQ.049hpbiQsCbEHLmc0e91lw';
 var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/outdoors-v9',
-    center: [4.3819917, 50.7694164],
-    zoom: 8
+  container: 'map',
+  style: 'mapbox://styles/mapbox/outdoors-v9',
+  center: [4.3819917, 50.7694164],
+  zoom: 8
 });
 
+
+// createGeoJSONCircle by Brad Dwyer from
+// https://stackoverflow.com/questions/37599561/drawing-a-circle-with-the-radius-in-miles-meters-with-mapbox-gl-js
+// Licenced under Creative Commons Attribution-Share Alike.
 var createGeoJSONCircle = function(center, radiusInKm, points) {
     if(!points) points = 64;
 
@@ -49,7 +53,6 @@ var createGeoJSONCircle = function(center, radiusInKm, points) {
 // Next opening
 var date_now = moment();
 var day_of_week = date_now.isoWeekday();
-
 var tonight = moment().add(1, 'days').hours(0).minutes(0);
 
 function next_passage(m) {
@@ -94,166 +97,167 @@ function update_data(data) {
 }
 
 
-
+// Load markers and populate the map
 map.on('load', function() {
 $.getJSON("assets/markers.geojson", function(data){
-    map.addSource("boxes", {
-        type: "geojson",
-        data: update_data(data),
-        cluster: true,
-        clusterMaxZoom: 10, // Max zoom to cluster points on
-        clusterRadius: 100 // Radius of each cluster when clustering points (defaults to 50)
-    });
+  map.addSource("boxes", {
+    type: "geojson",
+    data: update_data(data),
+    cluster: true,
+    clusterMaxZoom: 10, // Max zoom to cluster points on
+    clusterRadius: 100 // Radius of each cluster when clustering points (defaults to 50)
+  });
+
+  // Single point for user location
+  map.addSource('single-point', {
+    "type": "geojson",
+    "data": {
+      "type": "FeatureCollection",
+      "features": []
+    }
+  });
+  map.addLayer({
+    "id": "point",
+    "source": "single-point",
+    "type": "symbol",
+    "layout": {
+      "icon-image": "rocket-15",
+      "icon-size": 1.5
+    },
+    "paint": {
+      "text-color": "#222222"
+    },
+  });
+
+  // Circle around the user location
+  map.addSource("polygon", createGeoJSONCircle([0, 0], 0));
+  map.addLayer({
+    "id": "polygon",
+    "type": "fill",
+    "source": "polygon",
+    "layout": {},
+    "paint": {
+      "fill-color": "#1899da",
+      "fill-opacity": 0.3
+    }
+  });
 
 
-    map.addSource('single-point', {
-        "type": "geojson",
-        "data": {
-            "type": "FeatureCollection",
-            "features": []
-        }
-    });
-
-    map.addLayer({
-        "id": "point",
-        "source": "single-point",
-        "type": "symbol",
-        "layout": {
-            "icon-image": "rocket-15",
-            "icon-size": 1.5
-        },
-        "paint": {
-          "text-color": "#222222"
-        },
-    });
-
-    map.addSource("polygon", createGeoJSONCircle([-93.6248586, 41.58527859], 0.5));
-    map.addLayer({
-          "id": "polygon",
-          "type": "fill",
-        "source": "polygon",
-        "layout": {},
-        "paint": {
-            "fill-color": "#1899da",
-            "fill-opacity": 0.3
-        }
-    });
-
-
-    map.addLayer({
-        id: "clusters",
-        type: "circle",
-        source: "boxes",
-        filter: ["has", "point_count"],
-        paint: {
-            "circle-color": {
-                property: "point_count",
-                type: "interval",
-                stops: [
-                    [0, "#51bbd6"],
-                    [100, "#f1f075"],
-                    [750, "#f28cb1"],
-                ]
-            },
-            "circle-radius": {
-                property: "point_count",
-                type: "interval",
-                stops: [
-                    [0, 20],
-                    [100, 30],
-                    [750, 40]
-                ]
-            }
-        }
-    });
-
-    map.addLayer({
-      id: "cluster-count",
-      type: "symbol",
-      source: "boxes",
-      filter: ["has", "point_count"],
-      layout: {
-          "text-field": "{point_count_abbreviated}",
-          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
-          "text-size": 12
+  map.addLayer({
+    id: "clusters",
+    type: "circle",
+    source: "boxes",
+    filter: ["has", "point_count"],
+    paint: {
+      "circle-color": {
+        property: "point_count",
+        type: "interval",
+        stops: [
+          [0, "#51bbd6"],
+          [100, "#f1f075"],
+          [750, "#f28cb1"],
+        ]
+      },
+      "circle-radius": {
+        property: "point_count",
+        type: "interval",
+        stops: [
+          [0, 20],
+          [100, 30],
+          [750, 40]
+        ]
       }
-    });
+    }
+  });
 
-    map.addLayer({
-      id: "unclustered-point",
-      type: "circle",
-      source: "boxes",
-      filter: ["!has", "point_count"],
+  map.addLayer({
+    id: "cluster-count",
+    type: "symbol",
+    source: "boxes",
+    filter: ["has", "point_count"],
+    layout: {
+      "text-field": "{point_count_abbreviated}",
+      "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+      "text-size": 12
+    }
+  });
 
-      'paint': {
-          // make circles larger as the user zooms from z12 to z22
-          'circle-radius': {
-              'base': 1.75,
-              'stops': [[12, 2], [22, 180*2.5]]
-          },
-          // color circles by ethnicity, using data-driven styles
-          'circle-color': {
-              property: 'open_today',
-              type: 'categorical',
-              stops: [
-                  [false, '#cccccc'],
-                  [true, '#e22f39']
-              ]
-          },
+  map.addLayer({
+    id: "unclustered-point",
+    type: "circle",
+    source: "boxes",
+    filter: ["!has", "point_count"],
 
-          'circle-stroke-width': 2,
-          'circle-stroke-color': {
-              property: 'open_today',
-              type: 'categorical',
-              stops: [
-                  [false, '#aaaaaa'],
-                  [true, '#99151b']
-              ]
-          },
-      }
-    });
+    'paint': {
+      'circle-radius': {
+        'base': 1.75,
+        'stops': [[12, 2], [22, 180*2.5]]
+      },
+      'circle-color': {
+        property: 'open_today',
+        type: 'categorical',
+        stops: [
+          [false, '#cccccc'],
+          [true, '#e22f39']
+        ]
+      },
+      'circle-stroke-width': 2,
+      'circle-stroke-color': {
+          property: 'open_today',
+          type: 'categorical',
+          stops: [
+            [false, '#aaaaaa'],
+            [true, '#99151b']
+          ]
+      },
+    }
   });
 });
+});
 
-  var nav = new mapboxgl.NavigationControl();
-  map.addControl(nav, 'top-left');
 
-  // Geolocation
-  var geoLocate = new mapboxgl.GeolocateControl();
-  map.addControl(geoLocate);
-  geoLocate.on('geolocate', function(e) {
-    var coords = [e.coords.longitude, e.coords.latitude];
+// Add navigation and geolocation controls
+var nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-left');
 
-    map.getSource('single-point').setData({
-        "type": "Point",
-        "coordinates": coords
-    });
+// Geolocation
+var geoLocate = new mapboxgl.GeolocateControl();
+map.addControl(geoLocate);
+geoLocate.on('geolocate', function(e) {
+  var coords = [e.coords.longitude, e.coords.latitude];
 
-    map.getSource('polygon').setData(createGeoJSONCircle(coords, 1).data);
-    map.setZoom(13);
+  map.getSource('single-point').setData({
+      "type": "Point",
+      "coordinates": coords
   });
 
-  map.on('mouseenter', 'unclustered-point', function () {
-      map.getCanvas().style.cursor = 'pointer';
-  });
-  map.on('mouseleave', 'unclustered-point', function () {
-      map.getCanvas().style.cursor = '';
-  });
+  map.getSource('polygon').setData(createGeoJSONCircle(coords, 1).data);
+  map.setZoom(13);
+});
 
-  map.on('click', 'unclustered-point', function (e) {
-    var props = e.features[0].properties;
 
-    var html_content = props.address_fr.replace("\n", "<br />") +
-                       "<br />Mon-Fri: " +
-                       props.hours_week + ":" + props.minutes_week + "<br />Sat: ";
+// Add popup when clicking on an unclustered point
+map.on('mouseenter', 'unclustered-point', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+map.on('mouseleave', 'unclustered-point', function () {
+    map.getCanvas().style.cursor = '';
+});
 
-    if (props.hours_sat != "null")
-      html_content += props.hours_sat + ":" + props.minutes_sat;
-    else
-      html_content += "no schedule"
+map.on('click', 'unclustered-point', function (e) {
+  var props = e.features[0].properties;
 
-    new mapboxgl.Popup()
-        .setLngLat(e.features[0].geometry.coordinates)
-        .setHTML(html_content)
-        .addTo(map);
-  });
+  var html_content = props.address_fr.replace("\n", "<br />") +
+                     "<br />Mon-Fri: " +
+                     props.hours_week + ":" + props.minutes_week + "<br />Sat: ";
+
+  if (props.hours_sat != "null")
+    html_content += props.hours_sat + ":" + props.minutes_sat;
+  else
+    html_content += "no schedule"
+
+  new mapboxgl.Popup()
+    .setLngLat(e.features[0].geometry.coordinates)
+    .setHTML(html_content)
+    .addTo(map);
+});

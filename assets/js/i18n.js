9006b3caf829c2d1b6d920c029a0a45fd98416aa
Eric Schrijver <eric@ericschrijver.nl>
2017-05-08 02:09:48 +0200

    Design tweaks

--- assets/js/i18n.js
+++ assets/js/i18n.js
@@ -10,24 +10,24 @@ if (browserLang == 'nl') {
 var i18n = {
     en: {
         'title' : 'Find a red letterbox near you',
-        'pitch' : 'Belgian red letterboxes: you never know quite where they are and when they get serviced. Now there’s an app that tells you:',
+        'pitch' : 'Belgian red letterboxes: you never know quite where they are and when they get serviced. Now there’s an app that tells you.',
         'action': 'Find a red box!',
         'and' : 'And',
-        'read-blog-post': 'read the blog post on how we did it'
+        'read-blog-post': 'Read the blog post on how we did it'
     },
     fr: {
         'title' : 'Trouvez une boîte aux lettres rouge près de vous',
         'pitch' : 'Les boîtes aux lettres rouges belges: vous ne savez jamais vraiment où elles se trouvent et quand elles sont relevées? Il existe maintenant une application qui vous l’indique.',
-        'action': 'Trouvez une boîte à lettres rouge!',
+        'action': 'Trouvez une boîte aux lettres rouge!',
         'and' : 'Et',
-        'read-blog-post': 'lisez l’article de blog sur la façon dont nous avons construit l’application'
+        'read-blog-post': 'Lisez l’article de blog sur la façon dont nous avons construit l’application'
     },
     nl: {
         'title' : 'Vind een rode brievenbus dichtbij',
         'pitch' : 'De rode brievenbussen in België: Waar zijn ze? en hoe laat worden ze gelicht? Nu is er een app die het weet!',
         'action': 'Vind een rode brievenbus!',
         'and' : 'En',
-        'read-blog-post': 'hier lees je hoe en waarom we de app gebouwd hebben'
+        'read-blog-post': 'Hier lees je hoe en waarom we de app gebouwd hebben'
     }
 };
 $('[data-i18n]').each(function(i, el) {
